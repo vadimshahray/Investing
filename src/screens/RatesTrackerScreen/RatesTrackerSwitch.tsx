d23f3f0815b12f0092b318705a2981bda5a58a30
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2023-01-22 02:59:59 +0700

    feat: switch rates tracker is tracking

--- src/screens/RatesTrackerScreen/RatesTrackerSwitch.tsx
+++ src/screens/RatesTrackerScreen/RatesTrackerSwitch.tsx
@@ -1,6 +1,28 @@
 import React from 'react'
+import { useDispatch } from '@hooks'
+import { DefaultTheme } from '@styles'
+import { setIsTracking } from '@slices'
+import { useSelector } from 'react-redux'
 import { Button } from 'react-native-paper'
+import { selectRatesTrackerIsTracking } from '@selectors'
 
 export const RatesTrackerSwitch = () => {
-  return <Button mode='contained-tonal'>Запустить</Button>
+  const dispatch = useDispatch()
+
+  const isTracking = useSelector(selectRatesTrackerIsTracking)
+
+  const switchTracker = () => {
+    dispatch(setIsTracking(!isTracking))
+  }
+
+  return (
+    <Button
+      mode='contained-tonal'
+      onPress={switchTracker}
+      textColor={isTracking ? DefaultTheme.colors.error : undefined}
+      buttonColor={isTracking ? DefaultTheme.colors.errorContainer : undefined}
+    >
+      {isTracking ? 'Остановить' : 'Запустить'}
+    </Button>
+  )
 }
